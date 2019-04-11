a24f602cd697e61195680f1a748a3742ffd2763a
Marcus Wiseman <marcus.e.wiseman@gmail.com>
2019-04-11 03:41:36 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   } else if (string === string.toUpperCase() ) {
     return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
-    return "I love you too."
+    return "I love you, too."
   } else {
     return "code didnt work"
   }
